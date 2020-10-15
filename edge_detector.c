198286701194143bfa3cb7de317ee303993b1c07
swcwjy <60815911+swcwjy@users.noreply.github.com>
2020-10-15 15:55:16 +1100

    Update and rename edge_detector to edge_detector.c

---
+++ edge_detector.c
@@ -0,0 +1,15 @@
+// Edge detector to determine shape of object
+
+
+const int locator = A1; // analogue input for locator
+
+//within void setup 
+ // code for location
+  pinMode(locator,INPUT);
+  
+  
+//within void loop
+int proximityADC = analogRead(locator);
+  float proximityV = (float)proximityADC * 5.0 / 1023.0;
+  Serial.println(proximityV);
+  delay(100);
