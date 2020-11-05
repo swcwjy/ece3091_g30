454174cc60741f6de232dfa8c3b87566823ab5e9
swcwjy <60815911+swcwjy@users.noreply.github.com>
2020-11-05 14:39:08 +1100

    Update main.c
    final code

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-
+ 
 
 //#include <VarSpeedServo.h>
 
@@ -12,11 +12,11 @@
 float jointAngles[3];
 float endEffectorPos[3];
 float proximityV;
-float prox = 3.5;
-int light, i;
-int cube_size = 0;
-int light_arr[8];
+float prox = 2;
+int light,i;
+int cubesize = 0;
 int count_loop = 0;
+int pick_up = 0;
 int r = 0;
 int g = 0;
 int b = 0;
@@ -24,6 +24,9 @@ int reset = 0;
 int flag = 0;
 int colour_determine = 0;
 int prox_on_off = 0;
+int claw_close = 14;
+  int claw_open = 80;
+
 const int L1 = 6;
 const int L2 = 8;
 const int L3 = 8;
@@ -150,24 +153,8 @@ void calc_IK(float x1,float y1,float z1)
   Serial.println(th3);
   //*/
 }
-void swap(int *p,int *q) {
-   int t;
-   
-   t=*p; 
-   *p=*q; 
-   *q=t;
-}
 
-void sort(int a[],int n) { 
-   int i,j,temp;
 
-   for(i = 0;i < n-1;i++) {
-      for(j = 0;j < n-i-1;j++) {
-         if(a[j] > a[j+1])
-            swap(&a[j],&a[j+1]);
-      }
-   }
-}
 void move_IK(float x, float y, float z)
 {
   calc_IK(x,y,z);
@@ -186,22 +173,22 @@ void move_angle(float theta1,float theta2,float theta3)
   servo2.slowmove(theta2,10);
   servo3.slowmove(theta3,10);
   delay(200);
-  ///*
+  /*
   Serial.println(theta1);
   Serial.println(theta2);
   Serial.println(theta3);
-  //*/
+  */
 }
 
 void panning_height_top()
 {
-  servo2.slowmove(76-75+90,10);
-  servo3.slowmove(52+25,10);
+  servo2.slowmove(74-75+90,10);
+  servo3.slowmove(54+25,10);
   delay(300);
 }
 void panning_height_middle()
 {
-  servo2.slowmove(68-75+90,10);
+  servo2.slowmove(65-75+90,10);
   servo3.slowmove(40+25,10);
   delay(300);
 }
@@ -214,7 +201,8 @@ void panning_height_bottom()
 
 void move_up()
 {
-  servo2.slowmove(90+45-75,10);
+  servo1.slowmove(0,10);
+  servo2.slowmove(90-75,10);
   servo3.slowmove(90+45+25,10);
   delay(300);
 }
@@ -233,8 +221,8 @@ void move_down_middle() // y = 20, z just above cube
 }
 void move_down_bottom() // y = 20, z just above cube
 {
-  servo2.slowmove(62-75+90,10);
-  servo3.slowmove(14+25,10);
+  servo2.slowmove(64-75+90,10);
+  servo3.slowmove(15+25,10);
   delay(300);
 }
 
@@ -253,154 +241,229 @@ void proximity()
   //delay(100);
 }
 
-void colour_sensor_pos()
+void colour_sensor_pos(int cubesize)
 {
-  // hardcode position of colour sensor
-  servo1.slowmove(20,10)
-  servo2.slowmove(17-75+90,10);
-  servo3.slowmove(0+25,10);
+  if(cubesize ==0)// for big cube
+  {
+    servo1.slowmove(44,10);
+  servo2.slowmove(36 -75+90,10);
+  servo3.slowmove(43+25,10);
   delay(300);
-  
+  }
+  else 
+  servo1.slowmove(44,10);
+  servo2.slowmove(36 -75+90,10);
+  servo3.slowmove(44+25,10);
+  delay(300);
 }
 
 void container_pos(int r,int g, int b,int cube_size)
 {
   // locations need to be changed
-  if(cube_size ==1 && r ==1)
+  if(cube_size ==1 && r ==1) // small red cube
   {
-    servo1.slowmove(70,10);
-    servo2.slowmove(88-75+90,10);
-    servo3.slowmove(45+25,10);
+    servo1.slowmove(90-20,10);
+    servo2.slowmove(45 -75+90,10);
+    servo3.slowmove(36+25,10);
   }
-  if(cube_size ==0 && r ==1)
+  if(cube_size ==0 && r ==1) // big red cube
   {
-    servo1.slowmove(70,10);
-    servo2.slowmove(88-75+90,10);
-    servo3.slowmove(45+25,10);
+    servo1.slowmove(90-10,10);
+    servo2.slowmove(45-75+90,10);
+    servo3.slowmove(36+25,10);
   }
-  if(cube_size ==1 && g ==1)
+  if(cube_size ==1 && g ==1) // small green cube
   {
-    servo1.slowmove(70,10);
-    servo2.slowmove(88-75+90,10);
-    servo3.slowmove(45+25,10);
+    servo1.slowmove(90-20,10);
+    servo2.slowmove(50-75+90,10);
+    servo3.slowmove(47+25,10);
   }
-  if(cube_size ==0 && g ==1)
+  if(cube_size ==0 && g ==1) // big green cube
   {
-    servo1.slowmove(70,10);
-    servo2.slowmove(88-75+90,10);
-    servo3.slowmove(45+25,10);
+    servo1.slowmove(90-10,10);
+    servo2.slowmove(50-75+90,10);
+    servo3.slowmove(47 +25,10);
   }
-  if(cube_size ==1 && b ==1)
+  if(cube_size ==1 && b ==1) // small blue cube
   {
-    servo1.slowmove(70,10);
-    servo2.slowmove(88-75+90,10);
-    servo3.slowmove(45+25,10);
+    servo1.slowmove(90-20,10);
+    servo2.slowmove(80-75+90,10);
+    servo3.slowmove(82+25,10);
   }
-  if(cube_size ==0 && b ==1)
+  if(cube_size ==0 && b ==1) // big blue cube
   {
-    servo1.slowmove(70,10);
-    servo2.slowmove(88-75+90,10);
-    servo3.slowmove(45+25,10);
+    servo1.slowmove(90-10,10);
+    servo2.slowmove(80-75+90,10);
+    servo3.slowmove(82+25,10);
   }
   reset = 1;
 }
 
-void cube_size_detect()
-{
-  if( (digitalRead(Lswitch) == LOW) && (flag == 0) ) 
-  {
-    Serial.println("door is closed"); 
-    flag = 1; 
-    delay(20); 
-  }
-  
-    if( (digitalRead(Lswitch) == HIGH) && (flag == 1) ) 
-  {
-    Serial.println("door is opened"); 
-    flag = 0;
-    delay(20); 
-  }
-  
-  
-}
 
 void colour_sense()
 {
-  digitalWrite(2,HIGH);
-  digitalWrite(3,HIGH);
-  digitalWrite(4,HIGH);
+  int val = 8; // number of light readings
+  int light_arr[val],median;
+  
+  int samples = 0;
+  digitalWrite(10,HIGH); // red
+  digitalWrite(11,HIGH); // green
+  digitalWrite(12,HIGH); // blue
   delay(500);
-
-  while(light<30){delay(500);light = analogRead(A5);Serial.println(light);}
-  delay(1000);
+  
   light = analogRead(A5);
-  Serial.println(light);
-  //blue = ~50 - 70 , 4
-  //green =~150-170 , 3
-  //red = ~100-120 , 2
-  // nothing < 30
-  
-  switch(light)// flash each LED individually and record values reflected
+  while(light>900)
   {
-    // values below for low light conditions
-    case 50 ... 70: //blue
-    digitalWrite(2,LOW);
-    digitalWrite(3,LOW);
-    digitalWrite(4,HIGH);  
-    b = 1;
-    colour_determine = 1;
-    while(light>50){light = analogRead(A5);Serial.println(light); delay(500);}
-    break;
-    
-    case 120 ... 170: //green
-    digitalWrite(2,LOW);
-    digitalWrite(3,HIGH);
-    digitalWrite(4,LOW);  
-    g = 1;
-    colour_determine = 1;
-    while(light>50){light = analogRead(A5);Serial.println(light); delay(500);}
-    break;
-    break;
-    
-    case 90 ... 110: //red
-     digitalWrite(2,HIGH);
-    digitalWrite(3,LOW);
-    digitalWrite(4,LOW);  
-    r = 1;
-    colour_determine = 1;
-    while(light>40){light = analogRead(A5); Serial.println(light);delay(500);}
-    break;
-    
-    
-    default:  
-    break;
+    delay(1000);
+    light = analogRead(A5);
+    Serial.println(light);
+    samples = 0;
   }
-  // compare values and go to respective location
+  delay(1000);
   
+  // delaying light reading 
+  for(int i  = 0 ; i<8  ; i++)
+  {
+  light = analogRead(A5);
+  //light_arr[i] = light;
+  Serial.println(light);
+  delay(500);
+  }
+
+  /*
+  light_arr[] = {0,0,0,0,0,0,0,0};
+  for(int i  = 0 ; i<val ; i++)
+  {
+  light = analogRead(A5);
+  light_arr[i] = light;
+  Serial.println(light);
+  delay(200);
+
+
+    // taking median of light voltages
+    median = val;
+    sort(light_arr,median);
+    
+    median = (median + 1)/2-1;
+    Serial.print("Median: ");
+    Serial.println(light_arr[median]);
+*/
+
+    switch(light) // flash each LED individually and record values reflected
+    {
+      // values below for low light conditions
+      case 200 ... 520: //blue
+      digitalWrite(10,LOW);
+      digitalWrite(11,LOW);
+      digitalWrite(12,HIGH);  
+      b = 1;
+      Serial.println("blue");
+      colour_determine = 1;
+      break;
+      
+      case 521 ... 620: //red
+      digitalWrite(10,HIGH);
+      digitalWrite(11,LOW);
+      digitalWrite(12,LOW); 
+      Serial.println("red"); 
+      r = 1;
+      colour_determine = 1;
+      break;
+      
+      case 621 ... 730: //green
+      digitalWrite(10,LOW);
+      digitalWrite(11,HIGH);
+      digitalWrite(12,LOW); 
+      Serial.println("green"); 
+      g = 1;
+      colour_determine = 1;
   
-  //calibrate
-  //digitalWrite(2,HIGH);
-  //digitalWrite(3,HIGH);
-  //digitalWrite(4,HIGH);
-  //delay(500);
-  //light = analogRead(A5);
-  //Serial.println(light);
+      break;
+      
+      default:  
+      break;
+    }
   
+  delay(1000);
+
+}
+
+void default_move(int layer)
+{
+  for(int x = -70; x<-4; x = x+2)
+  {
+      if(layer == 1){panning_height_bottom();}
+      if(layer ==2){panning_height_middle();}
+      if(layer ==3){panning_height_top();}
+      
+      
+      // bend down pick up block
+      if(proximityV<prox && prox_on_off ==1)
+      {
+        servo1.slowmove(90-(x),10);
+        delay(2000);
+        servo1.slowmove(90-(x-9),10);
+        delay(2000);
+        servo4.slowmove(claw_open,15);  // open claw
+        delay(2000);
+        if(layer == 1){move_down_bottom();}// move downwards toward object (roughly z = 0, object on same plane as base of robot)
+        if(layer ==2){move_down_middle();}
+        if(layer ==3){move_down_top();}
+           
+        delay(2000);
+        servo4.slowmove(claw_close,15);    // close claw and hopefully grab object
+        delay(2000);
+        if(digitalRead(2) == LOW){cubesize = 1;Serial.print("small cube");}// if switch pressed
+        else cubesize = 0;
+        Serial.print("big cube");
+        delay(1000);
+        move_up();
+        pick_up = 1;
+        prox_on_off = 0;
+        break;
+      }
+      rotate_base(x);
+      delay(100);
+      proximity();
+      delay(100);
+      prox_on_off =1;
+    }
+}
+
+void claw(int open_close)
+{
+  
+  if(open_close == 0)
+  {
+    servo4.slowmove(claw_open,15);
+  }
+  else
+  servo4.slowmove(claw_close,15);
 }
 
 void setup()
 {
-  //Code here:
+
   Serial.begin(9600);
+
+  // initialise servo motor positions
+  servo1.slowmove(90,10);
+  servo2.slowmove(90-75,10);
+  servo3.slowmove(90+25,10);
+//  servo4.slowmove(80,10);
+
+  
   // set up motors 
   servo1.attach(9); // base motor output
   servo2.attach(8); // shoulder motor output
   servo3.attach(7); // elbow motor output
-  servo4.attach(6); //  claw motor output
+  servo4.attach(5); //  claw motor output
   
   pinMode(locator,INPUT);
   pinMode(colourSense,OUTPUT);
   pinMode(Lswitch, INPUT);
+  pinMode(2,INPUT);
+  
 
   // JA1 = 15 -> th2 = 90
   // JA2 = 125 -> th3 = 90
@@ -422,227 +485,95 @@ void setup()
 
 void loop()
 {
-  // Fully extended arm Angle[x,100,110]
-
-  // BASE ELEVATED 2.3CM, CUBES ELEVATED 1CM
-  
-  int crnt_pos1, crnt_pos2, crnt_pos3, crnt_pos4; // current position
-  float ldr_output;
-  int x,y,z,i,j,theta1,theta2,theta3;
-  float r_reflect,g_reflect,b_reflect;
-  int pick_up = 0;
-  
-
-  // LOCATION DETECTION
-  // Read in the ADC and convert it to a voltage:
-  ///*
-
-  // problem is need to be constantly checking for <1, void loop doesn't allow
-  // need function constantly checking and passing value of proximityV to be detected
  
-  
-  // if promixityV < 1 then stop and continue to colour sensor
-  
-  //*/
-  
-  // Limit switches size detection
-  // if switch pressed
-  /*
-  if(digitalRead(10)==LOW){
-    digitalWrite(13,HIGH);
-  }else{
-    digitalWrite(13,LOW);
-  }
-  */
+  move_angle(90,90,90);
+ delay(3000);
+  servo4.slowmove(10,15);
+  delay(4000);
+  if(digitalRead(2) == LOW){cubesize = 1;Serial.print("small cube");}// if switch pressed
+        else cubesize = 0;
 
-  
-  servo4.slowmove(4,20);
-  delay(1000);
-  for(int x = -70; x<0; x = x+2)
-  {
-      
-      panning_height_bottom();
-      
-      // bend down pick up block
-      if(proximityV<prox && prox_on_off ==1)
-      {
-        servo1.slowmove(90-(x),10);
-        delay(2000);
-        servo1.slowmove(90-(x-8),10);
-        delay(2000);
-        servo4.slowmove(80,20);  // open claw
-        delay(2000);
-        move_down_bottom();   // move downwards toward object (roughly z = 0, object on same plane as base of robot)
-        delay(2000);
-        servo4.slowmove(4,20);   // close claw and hopefully grab object
-        delay(2000);
-        move_up();
-        pick_up = 1;
-        prox_on_off = 0;
-        break;
-      }
-      rotate_base(x);
-      delay(100);
-      proximity();
-      delay(100);
-      prox_on_off =1;
-    }
-  prox_on_off = 0;
-  if(pick_up != 1)
-  {
-    servo4.slowmove(4,20);
-    for(int x = -70; x<0; x = x+2)
-    {
-      
-      panning_height_middle();
-      
-      // bend down pick up block
-      if(proximityV<prox&& prox_on_off ==1)
-      {
-        servo1.slowmove(90-(x),10);
-        delay(2000);
-        servo1.slowmove(90-(x-8),10);
-        delay(2000);
-        servo4.slowmove(80,20);  // open claw
-        delay(2000);
-        move_down_middle();   // move downwards toward object (roughly z = 0, object on same plane as base of robot)
-        delay(2000);
-        servo4.slowmove(4,20);   // close claw and hopefully grab object
-        delay(2000);
-        move_up();
-        pick_up = 1;
-        prox_on_off = 0;
-        break;
-      }
-      rotate_base(x);
-      delay(100);
-      proximity();
-      delay(100);
-      prox_on_off =1;
-     } 
-  }
-  
-  if(pick_up != 1)
-  {
-      servo4.slowmove(4,20);
-     for(int x = -70; x<0; x = x+2)
-    {
-      
-      panning_height_top();
-
-      // bend down pick up block
-      if(proximityV<prox&& prox_on_off ==1)
-      {
-        servo1.slowmove(90-(x),10);
-        delay(2000);
-        servo1.slowmove(90-(x-8),10);
-        delay(2000);
-        servo4.slowmove(80,20);  // open claw
-        delay(2000);
-        move_down_top();   // move downwards toward object (roughly z = 0, object on same plane as base of robot)
-        delay(2000);
-        servo4.slowmove(4,20);   // close claw and hopefully grab object
-        delay(2000);
-        move_up();
-        pick_up = 1;
-        prox_on_off = 0;
-        break;
-      }
-      rotate_base(x);
-      delay(100);
-      proximity();
-      delay(100);
-      prox_on_off =1;
-     } 
-  }
-    
-
-   if(pick_up ==1)
-  {
-    // Move to colour sensor + edge detector
-    r = 0; b = 0; g = 0;
     colour_determine = 0;
     while(colour_determine == 0)
     {
-      colour_sensor_pos();
-      delay(1000);
-      colour_sense();
-      delay(1000);
+   colour_sensor_pos(cubesize);
+    delay(3000);
+  colour_sense();
+ 
     }
-    
-    container_pos(r,g,b,cube_size);
-    delay(1000);
-    servo4.slowmove(80,20);  // open claw
-    delay(2000);
-    pick_up = 0;
-  }
-    
-  
-  
-  //*/
-  //move_IK(x,1,14);
-  
-  
-  //move_up();
-  //delay(4000);
-  /*
-  for(int x2 = -10; x2<0;x2 ++)
-  {
-    move_IK(x2,14,14);
-    
-    delay(500);
-  }
-  delay(500);
-  for(int x3 = 0; x3<-10;x3 ++)
-  {
-    move_IK(x3,14,14);
-    delay(500);
-    
-  }
-  delay(500);
-  */
+move_angle(90,90,90);
+  delay(3000);
+container_pos(r,g,b,cubesize);
+      delay(4000);
+      servo4.slowmove(80,20);  // open claw
+     delay(4000);
+move_angle(90,90,90);
+ delay(4000);
 
  
-  
-  // drawing a square
+  // Default panning state
+
   /*
-    servo4.slowmove(0,20);
+  servo4.slowmove(claw_close,15); 
+  delay(2000);
+  default_move(1);
+  prox_on_off = 0;
+  
+  if(pick_up != 1)
+  {
+    servo4.slowmove(claw_close,15); 
+    default_move(2);
+    if(pick_up != 1)
+    {
+       servo4.slowmove(claw_close,15); 
+       default_move(3);
+    }
+  }
+  
+  */
     
-  for(int x = -3; x<3; x ++)
-  {
-    move_IK(x,5,8);
+   
+  // Colour sensing and cube placement
   
-  }
-  delay(300);
-  for(int y = 5; y<12; y ++)
-  {
-    move_IK(3,y,8);
- 
-  }
-  delay(300);
-  for(int x2 = 3; x2>-3;x2 --)
-  {
-    move_IK(x2,12,8);
-  
-  }
-  delay(300);
-  
-  for(int y2 = 12; y2>5;y2 --)
-  {
-    move_IK(-3,y2,8);
- 
-  }
-  delay(300);
-  */
- 
-  // return to default
   /*
-  servo1.slowmove(pos1,10);
-  servo2.slowmove(pos2,10);
-  servo3.slowmove(pos3,10);
-  delay(7000);
+   if(pick_up ==1)
+    {
+      
+    // Move to colour sensor + edge detector
+    colour_determine = 0;
+    r = 0;b = 0; g = 0;
+    while(colour_determine == 0)
+    {
+      colour_sensor_pos();
+      delay(3000);
+      colour_sense();
   
+    }
+//    Serial.println("Red");
+//    Serial.println(r);
+//    Serial.println("Green");
+//    Serial.println(g);
+//    Serial.println("Blue");
+//    Serial.println(b);
+//    Serial.println("size");
+//    Serial.println(cubesize);
+//    
+    move_angle(90,90,90);
+    delay(3000);
+    container_pos(r,g,b,cubesize);
+    delay(3000);
+    claw(0); // open claw
+    delay(4000);
+     move_angle(90,90,90);
+     delay(2000);
+    pick_up = 0;
+    
+    
+  }
   */
+  
+  
+  
 
  
   }
